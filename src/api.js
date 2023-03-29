8727f5f286f0e6bcdf6b563cbe6165e16fff594f
MD. Jahdi Hossain Mridha <jahid4k@gmail.com>
2023-03-29 13:32:42 +0600

    testing

--- src/api.js
+++ src/api.js
@@ -1,16 +1,22 @@
-const express = require('express')
-const serverless = require('serverless-http')
+const express = require("express");
+const serverless = require("serverless-http");
 
+// Create an instance of the Express app
 const app = express();
-const router = express.Router()
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        Hello: "Hello Noobs"
-    })
-})
+// Create a router to handle routes
+const router = express.Router();
 
-app.use('/.netlify/functions/api', router)
+// Define a route that responds with a JSON object when a GET request is made to the root path
+router.get("/", (req, res) => {
+  res.json({
+    hello: "hi!"
+  });
+});
 
-module.exports.handler = serverless(app)
+// Use the router to handle requests to the `/.netlify/functions/api` path
+app.use(`/.netlify/functions/api`, router);
+
+// Export the app and the serverless function
+module.exports = app;
+module.exports.handler = serverless(app);
